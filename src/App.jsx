e8c1b9adefdd024da78724c53765f2e4a67ff29d
Filipov <andrijfilipov73@gmail.com>
2023-04-15 14:51:40 +0300

    make self todo ura

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-    const[todos, setTodos]=useState([])
-    const [text,setText]=useState('')
-
+    const [todos, setTodos] = useState([])
+    const [text, setText] = useState('')
 
     const addTodo = () => {
         setTodos([
@@ -14,50 +13,53 @@ function App() {
                 text,
                 comleted: false,
             },
-        ]);
+        ])
         setText('')
     }
 
     const removeTodo = (todoId) => {
-        setTodos(todos.filter(todo => todo.id !== todoId))
+        setTodos(todos.filter((todo) => todo.id !== todoId))
     }
 
     const toggleComleteTodo = (todoId) => {
-setTodos(todos.map(todo => {
-    if (todo.id !== todoId) return todo;
-    return {
-        ...todo,
-        comleted: !todo.comleted,
-
-    }}
-))
-    }
-    
-    
-
- return <div className="App">
-    <label htmlFor="">
-        <input type='text' onChange={(e) => setText(e.target.value)} />
-        <button onClick={addTodo}>add task</button>
-    </label>
-    <ul>
-        {todos.map(
-            todo => <li key={todo.id}>
-                <input 
-                checked={todo.comleted}
-                onChange={() => toggleComleteTodo(todo.id)}
-                className='checkbox'
-                type="checkbox" />
-                <span>{todo.text}</span>
-                <span className='delete' onClick={() => removeTodo(todo.id)}>&times;</span>
-            </li>
+        setTodos(
+            todos.map((todo) => {
+                if (todo.id !== todoId) return todo
+                return {
+                    ...todo,
+                    comleted: !todo.comleted,
+                }
+            })
         )
+    }
 
-        }
-    </ul>
- </div>
-
+    return (
+        <div className="App">
+            <label htmlFor="">
+                <input type="text" onChange={(e) => setText(e.target.value)} />
+                <button onClick={addTodo}>add task</button>
+            </label>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>
+                        <input
+                            checked={todo.comleted}
+                            onChange={() => toggleComleteTodo(todo.id)}
+                            className="checkbox"
+                            type="checkbox"
+                        />
+                        <p className="text">{todo.text}</p>
+                        <button
+                            className="delete"
+                            onClick={() => removeTodo(todo.id)}
+                        >
+                            &times;
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default App
-
