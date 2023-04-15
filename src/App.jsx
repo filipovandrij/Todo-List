670c57d243d0eddb75235853c532881729c73fc6
Filipov <andrijfilipov73@gmail.com>
2023-04-15 13:53:16 +0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,38 @@ import './App.css'
 
 function App() {
 
+    const[todos,setTodos] = useState([])
+    const[text,setText] = useState('')
 
+
+    const addTodo = () => {
+        setTodos([
+            ...todos,
+            {
+                id: ++todos.length,
+                text,
+                completed: false,
+            },
+        ]);
+        setText('')
+    }
+
+
+console.log(todos)
+
+return <div className="App">
+    <label htmlFor="">
+        <input type={text} onChange={(e) => setTodos(e.target.value)} />
+        <button onClick={() => addTodo()}>add task</button>
+    </label>
+    <ul>
+        <li>
+            <input type="checkbox" />
+            <span>text</span>
+            <span className='delete'>&times;</span>
+        </li>
+    </ul>
+</div>
 
 }
 
