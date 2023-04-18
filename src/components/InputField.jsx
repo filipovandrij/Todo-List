b09eeacae5ce58d7a278d86ad9326c808f230d77
Filipov <andrijfilipov73@gmail.com>
2023-04-18 16:44:11 +0300

    change on MUI

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,7 +1,14 @@
 import { useDispatch } from 'react-redux'
 import { addTodo } from '../redux/todoSlice'
-
-import { Button, TextField, MenuItem, Select } from '@mui/material'
+import {
+    Box,
+    FormControl,
+    InputLabel,
+    Button,
+    TextField,
+    MenuItem,
+    Select,
+} from '@mui/material'
 import { useState } from 'react'
 
 const InputField = () => {
@@ -15,24 +22,34 @@ const InputField = () => {
     }
 
     return (
-        <div>
-            <h3>Create Task</h3>
-            <Select label="Age" onChange={(e) => setImportant(e.target.value)}>
-                <MenuItem value="important">IMPORTANT</MenuItem>
-                <MenuItem value="medium">MEDIUM</MenuItem>
-                <MenuItem value="not-important">NOT-IMPORTANT</MenuItem>
-            </Select>
-            <TextField
-                label="Task name"
-                variant="outlined"
-                type="text"
-                value={text}
-                onChange={(e) => setTextValue(e.target.value)}
-            />
-            <Button variant="contained" onClick={() => addTask()}>
-                DONE
-            </Button>
-        </div>
+        <Box
+            sx={{ maxWidth: 1200, margin: '20px auto' }}
+            className="create_form"
+        >
+            <FormControl fullWidth sx={{ display: 'flex' }}>
+                <InputLabel id="demo-simple-select-label">Priority</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    value={important}
+                    label="Priority"
+                    onChange={(e) => setImportant(e.target.value)}
+                >
+                    <MenuItem value="important">IMPORTANT</MenuItem>
+                    <MenuItem value="medium">MEDIUM</MenuItem>
+                    <MenuItem value="not-important">NOT-IMPORTANT</MenuItem>
+                </Select>
+                <TextField
+                    label="Task name"
+                    variant="outlined"
+                    type="text"
+                    value={text}
+                    onChange={(e) => setTextValue(e.target.value)}
+                />
+                <Button variant="contained" onClick={() => addTask()}>
+                    DONE
+                </Button>
+            </FormControl>
+        </Box>
     )
 }
 export default InputField
